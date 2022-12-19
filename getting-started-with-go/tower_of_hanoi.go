e6bf49c3a79f737c87092d79659e6e88923c8047
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-12-19 21:00:21 +0100

    [ADD] tower of hanoi, the end of the world?

--- getting-started-with-go/tower_of_hanoi.go
+++ getting-started-with-go/tower_of_hanoi.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	hanoi(3, 0, 1, 2)
+	fmt.Printf("The number of moves are %d\n", hanoi(3, 0, 1, 2, true))
+	// fmt.Printf("The end of the world in %d days?\n", hanoi(64, 0, 1, 2, false)) // Yes it is... 2^64-1
 }
 
-func hanoi(n int, from int, tmp int, to int) {
+func hanoi(n int, from int, tmp int, to int, print bool) int {
 	if n == 1 {
-		fmt.Printf("%d -> %d\n", from, to)
-		return
+		if print {
+			fmt.Printf("%d -> %d\n", from, to)
+		}
+		return 1
 	}
-	hanoi(n-1, from, to, tmp)
-	fmt.Printf("%d -> %d\n", from, to)
-	hanoi(n-1, tmp, from, to)
+	hanoi(n-1, from, to, tmp, print)
+	if print {
+		fmt.Printf("%d -> %d\n", from, to)
+	}
+	return 2*hanoi(n-1, tmp, from, to, print) + 1
 }
